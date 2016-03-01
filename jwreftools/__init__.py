33a0430f9155c42d7224f3c09f0f347cf4b18542
Nadia Dencheva <nadia.dencheva@gmail.com>
2016-03-01 13:56:16 -0500

    add __init__ file

--- jwreftools/__init__.py
+++ jwreftools/__init__.py
@@ -12,7 +12,7 @@ from ._astropy_init import *
 
 # For egg_info test builds to pass, put package imports here.
 if not _ASTROPY_SETUP_:
+    from . import niriss
     from . import nirspec
     from . import nircam 
-    from . import niriss
     from . import miri
