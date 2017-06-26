8ca03d07e5bb765772344deb1a29022cdefe94a9
Bryan Hilbert <hilbert@stsci.edu>
2017-06-26 15:33:00 -0400

    Scripts now produce distortion reference files in Build 7.1 format

--- jwreftools/nircam/make_all_distortion_reffiles.py
+++ jwreftools/nircam/make_all_distortion_reffiles.py
@@ -2,8 +2,8 @@ import nircam_reftools as ref
 from astropy.io import ascii
 import numpy as np
 
-siaf_file = 'NIRCam_SIAF-OSS_Summary_20161117_MMA.csv'
-nrc_siaf = 'NIRCam_SIAF_2016-11-21.csv'
+siaf_file = 'NIRCam_SIAF-OSS_Summary_20170329_MMA.csv'
+nrc_siaf = 'NIRCam_SIAF_2017-03-28.csv'
 
 siaf = ascii.read(siaf_file,data_start=18)
 siaf['col4'].fill_value = 'na'
@@ -18,53 +18,135 @@ for mod in mods:
     for i in range(5):
         apertures.append('NRC'+mod+str(i+1)+'_FULL')
 
-coron = ['NRCA2_FULL_MASK210R','NRCA5_FULL_MASK335R','NRCA4_FULL_MASKSWB','NRCA5_FULL_MASKLWB']
-grism = ['NRCA5_GRISM_F322W2']
-apertures = apertures + coron + grism
-
 #FOR TESTING
-apertures = ['NRCA1_FULL','NRCA2_FULL','NRCA3_FULL','NRCA4_FULL','NRCA5_FULL',
-             'NRCB1_FULL','NRCB2_FULL','NRCB3_FULL','NRCB4_FULL','NRCB5_FULL']
+grism_apertures = ['NRCA5_GRISM_F322W2']
 
-        
-for entry in apertures:
+
+#necessary IMAGING metadata-----------------------------------------------
+imaging_apertures = ['NRCA1_FULL','NRCA2_FULL','NRCA3_FULL','NRCA4_FULL'
+                     ,'NRCA5_FULL',
+                     'NRCB1_FULL','NRCB2_FULL','NRCB3_FULL','NRCB4_FULL'
+                     ,'NRCB5_FULL']
+sw_imaging_pupil = ['CLEAR','F162M','F164N','GDHS0','GDHS60','WLM8'
+                    ,'WLP8','PINHOLES','MASKIPR']
+lw_imaging_pupil = ['CLEAR','F323N','F405N','F466N','F470N','PINHOLES'
+                    ,'MASKIPR']
+
+sw_imaging_subarr = ['FULL','SUB64P','SUB160','SUB160P','SUB320','SUB400P'
+                     ,'SUB640','SUB8FP1A','SUB8FP1B','SUB64FP1A','SUB64FP1B'
+                     ,'SUB96DHSPILA','SUB96DHSPILB']
+lw_imaging_subarr = ['FULL','SUB64P','SUB160','SUB160P','SUB320','SUB400P'
+                     ,'SUB640','SUB32TATS','SUB32TATSGRISM']
+
+imaging_exptype = ['NRC_IMAGE','NRC_TSIMAGE','NRC_FLAT','NRC_LED'
+                   ,'NRC_WFSC']
+#-------------------------------------------------------------------------
+
+#necessary CORONAGRAPHY metadata------------------------------------------
+
+#SW ROUND, A2
+sw_around_apertures = 'NRCA2_FULL_MASK210R'
+sw_around_pupil = ['MASKRND']
+sw_around_subarr = ['FULL','SUB640A210R','SUBNDA210R','SUBFSA210R']
+
+#SW BAR, A4
+sw_abar_apertures = 'NRCA4_FULL_MASKSWB'
+sw_abar_pupil = ['MASKBAR']
+sw_abar_subarr = ['FULL','SUB640ASWB','SUBFSASWB','SUBNDASWBL','SUBNDASWBS']
+
+#LW BAR, A5
+lw_abar_apertures = 'NRCA5_FULL_MASKLWB'
+lw_abar_pupil = ['MASKBAR']
+lw_abar_subarr = ['FULL','SUB320ALWB','SUBFSALWB','SUBNDALWBL','SUBNDALWBS']
+
+#LW ROUND, A5
+lw_around_apertures = 'NRCA5_FULL_MASK335R'
+lw_around_pupil = ['MASKRND']
+lw_around_subarr = ['FULL','SUB320A335R','SUB320A430R','SUBFSA335R'
+                    ,'SUBFSA430R','SUBNDA335R','SUBNDA430R']
+
+#SW BAR, B3
+#sw_bbar_apertures = ['NRCB3_MASKSWB']
+#sw_bbar_pupil = ['MASKSWBAR']
+#sw_bbar_subarr = ['FULL','SUB640BSWB','SUBNDBSWBL','SUBNDBSWBS']
+
+#SW ROUND, B1
+#sw_bround_apertures = ['NRCB1_MASK210R']
+#sw_bround_pupil = ['MASKSWRND']
+#sw_bround_subarr = ['FULL','SUB640B210R','SUBNDB210R']
+
+#LW BAR, B5
+#lw_bbar_apertures = ['NRCB5_MASKLWB']
+#lw_bbar_pupil = ['MASKLWB']
+#lw_bbar_subarr = ['FULL','SUBNDBLWBL','SUBNDBLWBS']
+
+#LW ROUND, B5
+#lw_bround_apertures = ['NRCB5_MASK335R']
+#lw_bround_pupil = ['MASKLWRND']
+#lw_bround_subarr = ['FULL','SUB320B335R','SUB320B430R','SUBNDB335R','SUBNDB430R']
+
+coron_exptypes = ['NRC_CORON','NRC_TACONFIRM','NRC_TACQ']
+
+
+#coron_apertures = ['NRCA2_MASK210R','NRCA5_MASK335R',
+#                   'NRCA4_MASKSWB','NRCA5_MASKLWB',
+#                   'NRCB1_MASK210R','NRCB5_MASK335R',
+#                   'NRCB3_MASKSWB','NRCB5_MASKLWB']
+#swbar_pupil = ['MASKSWBAR']
+#swbar_subarr = ['FULL','SUB640ASWB','SUB640BSWB','SUBFSASWB'
+#                ,'SUBNDASWBL','SUBNDASWBS','SUBNDBSWBL','SUBNDBSWBS']
+#swbar_exptype = ['NRC_CORON','NRC_TACONFIRM','NRC_TACQ']
+
+#swrnd_pupil = ['MASKSWRND']
+#swrnd_subarr = ['FULL','SUB640A210R','SUB640B210R','SUBFSA210R'
+#                ,'SUBNDA210R','SUBNDB210R']
+#swrnd_exptype = ['NRC_CORON','NRC_TACONFIRM','NRC_TACQ']
+
+#lwbar_pupil = ['MASKLWBAR']
+#lwbar_subarr = ['FULL','SUB320ALWB','SUB320BLWB','SUBFSALWB','SUBNDALWBL'
+#                ,'SUBNDALWBS','SUBNDBLWBL','SUBNDBLWBS']
+#lwbar_exptype = ['NRC_CORON','NRC_TACONFIRM','NRC_TACQ']
+
+#lwrnd_pupil = ['MASKLWRND']
+#lwrnd_subarr = ['FULL','SUB320A335R','SUB320A430R','SUB320B335R'
+#                ,'SUB320B430R','SUBFSA335R','SUBFSA430R','SUBNDA335R'
+#                ,'SUBNDA430R','SUBNDB335R','SUBNDB430R']
+#lwrnd_exptype = ['NRC_CORON','NRC_TACONFIRM','NRC_TACQ']
+#-------------------------------------------------------------------------
+
+
+#RUN IMAGING
+for entry in imaging_apertures:
     det = entry[0:5]
     aper_minus_det = entry[6:]
 
-    exp_type = 'NRC_IMAGE'
-    pupil = 'ANY'
-    if 'MASK' in entry:
-        exp_type = 'NRC_CORON'
-        m = entry.find('MASK')
-        mtype = entry[m+4:]
-        if 'R' in mtype:
-            pupil = 'MASKRND'
-        elif 'B' in mtype:
-            pupil = 'MASKBAR'
-        else:
-            print('Cannot determine type of mask for {}'.format(entry))
-            sys.exit()
-    elif 'GRISM' in entry:
-        exp_type = 'NRC_GRISM'
-        pupil = 'GRISMR'
+    if np.int(det[-1]) < 5:
+        pupil = sw_imaging_pupil
+        subarr = sw_imaging_subarr
+    else:
+        pupil = lw_imaging_pupil
+        subarr = lw_imaging_subarr
+        
+    exp_type = imaging_exptype
 
     outname = det + '_' + aper_minus_det + '_distortion.asdf'
-    print('running {}'.format(entry))
-    ref.create_nircam_distortion(nrc_siaf,det,aper_minus_det,exp_type,pupil,outname)
+    #print('running {}'.format(entry))
+    #print('det is {}'.format(np.int(det[-1])))
+    #print('pupil is {}'.format(pupil))
+    ref.create_nircam_distortion(nrc_siaf,det,aper_minus_det,outname,pupil,subarr,exp_type)
     
 
-#for entry in siaf:
-#    apername = entry['col4']
-#    opgs = str(entry['col18'])
-#    if apername != 'na' and opgs != 'na' and 'GRISMR' not in apername and 'GRISMC' not in apername:
-#        aper_minus_det = apername[6:]
-#        det = apername[0:5]
-#        try:
-#            detnum = np.int(det[-1])
-#            outname = 'reffiles_27Oct2016/' + det + '_' + aper_minus_det + '_distortion.asdf'
-#            print('running {},{}'.format(apername,opgs))
-#            ref.create_nircam_distortion('NIRCam_SIAF_2016-09-29.csv',det,aper_minus_det,opgs,outname)
-#        except:
-#            #print('skipping {}.'.format(entry))
-#            pass
+#RUN CORONAGRAPHY - NOTE!!! CORONAGRAPHY IS ONLY SUPPORTED IN MODULE A!
+
+c_apertures = [sw_around_apertures,sw_abar_apertures,lw_around_apertures,lw_abar_apertures]
+c_pupil = [sw_around_pupil,sw_abar_pupil,lw_around_pupil,lw_abar_pupil]
+c_subarr = [sw_around_subarr,sw_abar_subarr,lw_around_subarr,lw_abar_subarr]
+
+for ap,pup,sub in zip(c_apertures,c_pupil,c_subarr):
+    det = ap[0:5]
+    aper_minus_det = ap[6:]
+    outname = ap + '_distortion.asdf'
+    ref.create_nircam_distortion(nrc_siaf,det,aper_minus_det,outname,pup,sub,coron_exptypes)
         
+
+    
--- jwreftools/nircam/nircam_reftools.py
+++ jwreftools/nircam/nircam_reftools.py
@@ -1,5 +1,7 @@
+#! /usr/bin/env python
 """
-This module contains functions to create NIRCAM reference files.
+This module contains functions to create NIRCAM distortion reference 
+files using the DistortionModel model.
 
 NIRCAM model:
 im.meta.instrument.name :'NIRCAM'
@@ -8,48 +10,233 @@ im.meta.instrument.module : 'B'
 im.meta.instrument.detector : 'NRCB1'
 im.meta.exposure.type : 'NRC_IMAGE'
 
-???
-im.meta.instrument.pupil : 'FLAT' (would be GRISMR or GRISMC for slitless)
+im.meta.instrument.pupil : 'FLAT' (would be GRISMR or GRISMC for 
+slitless)
 
 Transform Paths for Imaging mode:
 
 science --> ideal --> V2V3 
 V2V3 --> ideal --> science
 
-Where the "science" frame has units of distorted pixels. The "ideal" frame
-is distortion-free and is the distance in arcseconds from 0,0. V2/V3 is also
-in units of arcseconds from the refrence pixel.
+Where the "science" frame has units of distorted pixels. The "ideal" 
+frame is distortion-free and is the distance in arcseconds from 0,0. 
+V2/V3 is also in units of arcseconds from the refrence pixel.
 
 
 """
 from asdf import AsdfFile
+import numpy as np
+import sys
 from astropy.modeling.models import Mapping,Shift
 from astropy.io import ascii
+from astropy import units as u
+from jwst.datamodels import DistortionModel,util
 import read_siaf_table
-import numpy as np
 
-def create_nircam_distortion(coefffile, detector, aperture, exp_type, pupil, outname):
+
+def create_nircam_distortion(coefffile, detector, aperture,
+                             outname, sci_pupil,
+                             sci_subarr, sci_exptype):
     """
-    Create an asdf reference file with all distortion components for the NIRCam imager.
+    Create an asdf reference file with all distortion components 
+    for the NIRCam imager.
 
-    NOTE: The IDT has not provided any distortion information. The files are constructed
-    using ISIM transformations provided/(computed?) by the TEL team which they use to
-    create the SIAF file.
-    These reference files should be replaced when/if the IDT provides us with distortion.
+    NOTE: The IDT has not provided any distortion information. The 
+    files are constructed using ISIM transformations provided by 
+    the TEL team which they use to create the SIAF file.
 
     Parameters
     ----------
     detector : str
-        NRCB1, NRCB2, NRCB3, NRCB4, NRCB5, NRCA1, NRCA2, NRCA3, NRCA4, NRCA5
+        NRCB1, NRCB2, NRCB3, NRCB4, NRCB5, NRCA1, NRCA2, NRCA3, 
+        NRCA4, NRCA5
     aperture : str
-        Name of the aperture/subarray. (e.g. FULL, SUB160, SUB320, SUB640, GRISM_F322W2)
+        Name of the aperture/subarray. (e.g. FULL, SUB160, SUB320, 
+        SUB640, GRISM_F322W2)
     outname : str
         Name of output file.
 
     Examples
     --------
-
     """
+
+    #Read in the SIAF table
+    siaf = ascii.read(coefffile,header_start=1)
+    
+    numdet = detector[-1]
+    module = detector[-2]
+    channel = 'SHORT'
+    if numdet == '5':
+        channel = 'LONG'
+        
+    full_aperture = detector + '_' + aperture
+    
+    #Find the row that matches the requested aperture
+    match = siaf['AperName'] == full_aperture
+    if np.any(match) == False:
+        print("Aperture name {} not found in input CSV file.".
+              format(aperture))
+        sys.exit()
+
+    siaf_row = siaf[match]
+    
+    #"Forward' transformations. science --> ideal --> V2V3
+
+    #Frist, find the offset between 0,0 and the reference
+    #location, in pixels
+    xshift,yshift = read_siaf_table.get_refpix(siaf_row)
+
+    #Now the core of the model. 2D-Polynomial to go from units
+    #of science pixels to the 'ideal' (distortion-free)
+    #coordinate system
+    sci2idlx, sci2idly, sciunit, idlunit = read_siaf_table.get_siaf_transform(siaf_row,full_aperture,'science','ideal', 5)
+
+    #And another 2D polynomial to go from 'ideal' coordinates
+    #to V2,V3
+    idl2v2v3x, idl2v2v3y = read_siaf_table.get_siaf_v2v3_transform(siaf_row,full_aperture,from_system='ideal')
+
+    #Finally, we need to shift by the v2,v3 value of the reference
+    #location in order to get to absolute v2,v3 coordinates
+    v2shift,v3shift = read_siaf_table.get_v2v3ref(siaf_row)
+    
+    #'Reverse' transformations. V2V3 --> ideal --> science
+    #In this case we just need the inverses of the polynomials
+    #from above
+    #Inverses of the shifts are known by astropy modeling 
+    v2v32idlx, v2v32idly = read_siaf_table.get_siaf_v2v3_transform(siaf_row,full_aperture,to_system='ideal')
+    idl2scix, idl2sciy, idlunit, sciunit = read_siaf_table.get_siaf_transform(siaf_row,full_aperture,'ideal','science', 5)
+
+    #Now create a compound model for each with the appropriate
+    #inverse
+    sci2idl = Mapping([0,1,0,1]) | sci2idlx & sci2idly
+    sci2idl.inverse = Mapping([0,1,0,1]) | idl2scix & idl2sciy
+
+    idl2v2v3 = Mapping([0,1,0,1]) | idl2v2v3x & idl2v2v3y
+    idl2v2v3.inverse = Mapping([0,1,0,1]) | v2v32idlx & v2v32idly
+    
+    #Now string the models together to make a single transformation
+    
+    #First the core of the translations, without the shifts
+    #Scale the models by 1/3600 because the SIAF has things in
+    #units of arcsec but the pipeline assumes degrees
+
+    #Official word is that reffiles should now work in arcsec,
+    #not degrees, so remove the Scale(1./3600), but we also need
+    #to account for the difference of 1 between the SIAF
+    #coordinate values (indexed to 1) and python (indexed to 0).
+    #Nadia said that this shift should be present in the
+    #distortion reference file.
+
+    core_model = sci2idl | idl2v2v3 
+    
+    #Now add in the shifts to create the full model
+    #including the shift to go from 0-indexed python coords to
+    #1-indexed
+    
+    #SIAF coords
+    index_shift = Shift(1)
+    model = index_shift & index_shift | xshift & yshift | core_model | v2shift & v3shift
+
+    #Since the inverse of all model components are now defined,
+    #the total model inverse is also defined automatically
+
+    #In the reference file headers, we need to switch NRCA5 to
+    #NRCALONG, and same for module B.
+    if detector[-1] == '5':
+        detector = detector[0:4] + 'LONG'
+
+    #Save using the DistortionModel datamodel
+    d = DistortionModel(model=model, input_units=u.pix,
+                        output_units=u.arcsec)
+
+    #Populate metadata
+
+    #keyword values in science data to which this file should
+    #be applied
+    p_pupil = ''
+    for p in sci_pupil:
+        p_pupil = p_pupil + p + '|'
+
+    p_subarr = ''
+    for p in sci_subarr:
+        p_subarr = p_subarr + p + '|'
+            
+    p_exptype = ''
+    for p in sci_exptype:
+        p_exptype = p_exptype + p + '|'
+
+    d.meta.instrument.p_pupil = p_pupil
+    d.meta.subarray.p_subarray = p_subarr
+    d.meta.exposure.p_exptype = p_exptype
+    
+    #d.meta.instrument.p_pupil = "CLEAR|F162M|F164N|F323N|F405N|F470N|"
+    #d.meta.p_subarray = "FULL|SUB64P|SUB160|SUB160P|SUB320|SUB400P|SUB640|SUB32TATS|SUB32TATSGRISM|SUB8FP1A|SUB8FP1B|SUB96DHSPILA|SUB96DHSPILB|SUB64FP1A|SUB64FP1B|"
+    #d.meta.exposure.p_exptype = "NRC_IMAGE|NRC_TSIMAGE|NRC_FLAT|NRC_LED|NRC_WFSC|"
+
+    #metadata describing the reference file itself
+    d.meta.title = "NIRCam Distortion"
+    d.meta.instrument.name = "NIRCAM"
+    d.meta.instrument.module = module
+    d.meta.instrument.channel = channel
+    d.meta.instrument.detector = detector
+    d.meta.telescope = 'JWST'
+    d.meta.subarray.name = 'FULL'
+    d.meta.pedigree = 'GROUND'
+    d.meta.reftype = 'DISTORTION' 
+    d.meta.author = 'B. Hilbert'
+    d.meta.litref = "https://github.com/spacetelescope/jwreftools"
+    d.meta.description = "Distortion model from SIAF coefficients in {}".format(coefffile)
+    #d.meta.exp_type = exp_type
+    d.meta.useafter = "2014-10-01T00:00:00"
+
+    #to be ready for the future where we will have filter-dependent solutions
+    d.meta.instrument.filter = 'N/A'
+
+    
+    #Create initial HISTORY ENTRY
+    
+    sdict = {'name':'nircam_reftools.py','author':'B.Hilbert','homepage':'https://github.com/spacetelescope/jwreftools','version':'0.71'}
+    
+    entry = util.create_history_entry("File created from distortion coefficients contained in SIAF ("+coefffile+"), provided by Colin Cox in March 2017. The format of this distortion reference file matches that used by version 7.1 of the JWST Calibration Pipeline. The translation model converts from units of pixels on the detector to V2,V3 in units of degrees, as well as the inverse. P_PUPIL values have been updated in these files to reflect the correct pupil wheel options.",software=sdict)
+    d.history = [entry]
+
+
+    #Create additional HISTORY entries
+    #entry2 = util.create_history_entry("The format of this distortion reference file matches that used by version 7.1 of the JWST Calibration Pipeline. The translation model converts from units of pixels on the detector to V2,V3 in units of degrees, as well as the inverse.")
+    #d.history.append(entry2)
+    
+
+    d.save(outname)
+    print("Output saved to {}".format(outname))
+
+
+
+def create_nircam_pixel_area_map(coefffile, detector, aperture,
+                             outname, sci_pupil,
+                             sci_subarr, sci_exptype):
+    """
+    Create an asdf reference file with all distortion components 
+    for the NIRCam imager.
+
+    NOTE: The IDT has not provided any distortion information. The 
+    files are constructed using ISIM transformations provided by 
+    the TEL team which they use to create the SIAF file.
+
+    Parameters
+    ----------
+    detector : str
+        NRCB1, NRCB2, NRCB3, NRCB4, NRCB5, NRCA1, NRCA2, NRCA3, 
+        NRCA4, NRCA5
+    aperture : str
+        Name of the aperture/subarray. (e.g. FULL, SUB160, SUB320, 
+        SUB640, GRISM_F322W2)
+    outname : str
+        Name of output file.
+
+    Examples
+    --------
+    """
+
     #Read in the SIAF table
     siaf = ascii.read(coefffile,header_start=1)
     
@@ -64,98 +251,39 @@ def create_nircam_distortion(coefffile, detector, aperture, exp_type, pupil, out
     #Find the row that matches the requested aperture
     match = siaf['AperName'] == full_aperture
     if np.any(match) == False:
-        print("Aperture name {} not found in input CSV file.".format(aperture))
+        print("Aperture name {} not found in input CSV file.".
+              format(aperture))
         sys.exit()
 
     siaf_row = siaf[match]
     
     #"Forward' transformations. science --> ideal --> V2V3
 
-    #Frist, find the offset between 0,0 and the reference location, in pixels
+    #Frist, find the offset between 0,0 and the reference
+    #location, in pixels
     xshift,yshift = read_siaf_table.get_refpix(siaf_row)
 
-    #Now the core of the model. 2D-Polynomial to go from units of science pixels
-    #to the 'ideal' (distortion-free) coordinate system
+    #Now the core of the model. 2D-Polynomial to go from units
+    #of science pixels to the 'ideal' (distortion-free)
+    #coordinate system
     sci2idlx, sci2idly, sciunit, idlunit = read_siaf_table.get_siaf_transform(siaf_row,full_aperture,'science','ideal', 5)
 
-    #And another 2D polynomial to go from 'ideal' coordinates to V2,V3
+    #And another 2D polynomial to go from 'ideal' coordinates
+    #to V2,V3
     idl2v2v3x, idl2v2v3y = read_siaf_table.get_siaf_v2v3_transform(siaf_row,full_aperture,from_system='ideal')
 
-    #Finally, we need to shift by the v2,v3 value of the reference location in order to get to
-    #absolute v2,v3 coordinates
+    #Finally, we need to shift by the v2,v3 value of the reference
+    #location in order to get to absolute v2,v3 coordinates
     v2shift,v3shift = read_siaf_table.get_v2v3ref(siaf_row)
     
     #'Reverse' transformations. V2V3 --> ideal --> science
-    #In this case we just need the inverses of the polynomials from above
+    #In this case we just need the inverses of the polynomials
+    #from above
     #Inverses of the shifts are known by astropy modeling 
     v2v32idlx, v2v32idly = read_siaf_table.get_siaf_v2v3_transform(siaf_row,full_aperture,to_system='ideal')
     idl2scix, idl2sciy, idlunit, sciunit = read_siaf_table.get_siaf_transform(siaf_row,full_aperture,'ideal','science', 5)
 
-
-    #Now create a compound model for each with the appropriate inverse
+    #Now create a compound model for each with the appropriate
+    #inverse
     sci2idl = Mapping([0,1,0,1]) | sci2idlx & sci2idly
     sci2idl.inverse = Mapping([0,1,0,1]) | idl2scix & idl2sciy
-
-    idl2v2v3 = Mapping([0,1,0,1]) | idl2v2v3x & idl2v2v3y
-    idl2v2v3.inverse = Mapping([0,1,0,1]) | v2v32idlx & v2v32idly
-    
-    #Now string the models together to make a single transformation
-    
-    #First the core of the translations, without the shifts
-    #Scale the models by 1/3600 because the SIAF has things in units of arcsec
-    #but the pipeline assumes degrees
-
-    #Official word is that reffiles should now work in arcsec,
-    #not degrees, so remove the Scale(1./3600), but we also need to
-    #account for the difference of 1 between the SIAF coordinate values
-    #(indexed to 1) and python (indexed to 0). Nadia said that this
-    #shift should be present in the distortion reference file.
-
-    core_model = sci2idl | idl2v2v3 #| Scale(1./3600) & Scale(1./3600)
-    
-    #Now add in the shifts to create the full model
-    #including the shift to go from 0-indexed python coords to 1-indexed
-    #SIAF coords
-    index_shift = Shift(1)
-    model = index_shift & index_shift | xshift & yshift | core_model | v2shift & v3shift
-
-    #Since the inverse of all model components are now defined, the total model
-    #inverse is also defined automatically
-
-    #core_model_inv =  Mapping([0, 1, 0, 1]) | v2v32idlx & v2v32idly | Mapping([0, 1, 0, 1]) | idl2scix & idl2sciy | Scale(1./3600) & Scale(1./3600)
-    #Now add in the inverse shifts
-    #model_inv = v2shift.inverse & v3shift.inverse | core_model_inv | xshift.inverse & yshift.inverse
-    #model.inverse = model_inv
-
-
-    #In the reference file headers, we need to switch NRCA5 to NRCALONG, and same
-    #for module B.
-    if detector[-1] == '5':
-        detector = detector[0:4] + 'LONG'
-
-    tree = {"TITLE": "NIRCAM Distortion",
-            "TELESCOP": "JWST",
-            "INSTRUMENT": "NIRCAM",
-            "PEDIGREE": "GROUND",
-            "REFTYPE" : "DISTORTION",
-            "AUTHOR": "B. Hilbert",
-            "LITREF": "https://github.com/spacetelescope/jwreftools",
-            "DETECTOR": detector,
-            "MODULE": module,
-            "CHANNEL": channel,
-            "PUPIL": pupil,
-            "SUBARRAY": 'ANY',
-            "DESCRIP": "Distortion model function created from SIAF coefficients",
-            "EXP_TYPE": exp_type,
-            "USEAFTER": "2014-01-01T00:00:00",
-            "model": model
-            }
-
-    fasdf = AsdfFile()
-    fasdf.tree = tree
-
-    sdict = {'name':'nircam_reftools.py','author':'B.Hilbert','homepage':'https://github.com/spacetelescope/jwreftools','version':'1.0'}
-    
-    fasdf.add_history_entry("File created from distortion coefficients contained in SIAF ("+coefffile+"), provided by Colin Cox in October 2016. Software used: https://github.com/spacetelescope/jwreftools",software=sdict)
-
-    fasdf.write_to(outname)
