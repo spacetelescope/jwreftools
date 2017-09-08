066b9eb6c2abc3aca56d61b78b433e60d7c2665f
Megan Sosey <sosey@stsci.edu>
2017-09-08 17:53:43 -0400

    move exp_type into the meta exposure for grism references

--- jwreftools/niriss/niriss_reftools.py
+++ jwreftools/niriss/niriss_reftools.py
@@ -82,7 +82,7 @@ def common_reference_file_keywords(reftype=None,
     ref_file_common_keywords = {
         "author": author,
         "description": description,
-        "exp_type": exp_type,
+        "exposure": {"type": exp_type},
         "instrument": {"name": "NIRISS",
                        "detector": "NIS"},
         "pedigree": "ground",
